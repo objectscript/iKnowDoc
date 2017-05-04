d436c8bc35f0ea69e984b609b3746a49888b2f53
manzhula <manzhula1994@gmail.com>
2017-05-04 15:18:40 +1000

    вывел пагинацию в отдельный сервис

--- CSP/csp/docsearch/js/app/controllers.js
+++ CSP/csp/docsearch/js/app/controllers.js
@@ -2,76 +2,6 @@ var searchApp = angular.module('searchApp');
  
 searchApp
 	
-	.factory('pagination', function( $sce ) {
-
-		var currentPage = 0;
-		var results = [];
-
-		return {		
-			
-			//
-			getPageProducts: function(num) {
-				var num = angular.isUndefined(num) ? 0 : num;
-				currentPage = num;
-			}, /* END of getPageProducts */
-			
-			//
-			getTotalPagesNum: function(TotalObjNum, itemsPerPage) {
-				return Math.ceil( TotalObjNum / itemsPerPage );
-			}, /* END of getTotalPagesNum */
-			
-			//
-			getPaginationList: function(TotalPagesNum, itemsPerPage) {
-				var pagesNum = this.getTotalPagesNum(TotalPagesNum, itemsPerPage);
-				var paginationList = [];
-				paginationList.push({
-					name: $sce.trustAsHtml('&laquo;'),
-					link: 'prev'
-				});
-									
-				for (var i = 0; i < pagesNum; i++) {
-					var name = i + 1;
-					paginationList.push({
-						name: $sce.trustAsHtml( String(name) ),
-						link: i
-					});
-				};
-				paginationList.push({
-					name: $sce.trustAsHtml('&raquo;'),
-					link: 'next'
-				});
-				if (pagesNum > 1) {
-					return paginationList;
-				} else {
-					return null;
-				}
-			}, /* END of getPaginationList */
-			
-			//
-			getPrevPageProducts: function() {
-				var prevPageNum = currentPage - 1;
-				if ( prevPageNum < 0 ) 
-					prevPageNum = 0;
-				return this.getPageProducts( prevPageNum );
-			}, /* END of getPrevPageProducts */
-			
-			//
-			getNextPageProducts: function(TotalPagesNum, itemsPerPage) {
-				var nextPageNum = currentPage + 1;
-				var pagesNum = this.getTotalPagesNum(TotalPagesNum, itemsPerPage);
-				if ( nextPageNum >= pagesNum )
-					nextPageNum = pagesNum - 1;
-				return this.getPageProducts( nextPageNum );
-			}, /* END of getNextPageProducts */
-			
-			//
-			getCurrentPageNum: function() {
-				return currentPage;
-			}, /* END of getCurrentPageNum */
-
-		}
-	}) /* END of factory-pagination */
-	
 	.controller('searchController', function ($scope, $http, pagination) {
 				
 		$scope.search = {
@@ -93,18 +23,23 @@ searchApp
 				$http.get('http://' + location.host + '/csp/docsearch/rest/GetSimilar/' + $scope.search.words)
 					.then(function(response) {
 							$scope.searchItems = response.data.entities;
-						});		
+						});	
+	
 				if ($scope.search.words == '')
 					$scope.imputToggle = false;
 				else 
 					$scope.imputToggle = true;
 		}
 		
-		$scope.handleClick = function (item){
+		$scope.handleClick = function (item) {
+		
 			$scope.currrentSearchItem = item;
-			$scope.search.text = $scope.currrentSearchItem.value;
+			$scope.search.words = $scope.currrentSearchItem.value;
+			$scope.makeSearch();
+			
 			$scope.toggle = false;
 			$scope.imputToggle = true;
+
 			if ($scope.toggle == false)
 					$scope.imputToggle = false;
 		}
@@ -160,7 +95,7 @@ searchApp
 				$scope.toggle = true;
 		}
 
-		$scope.showPage = function(page) {
+		$scope.showPage = function (page) {
 
 					if ( page == 'prev' ) {
 						$scope.results = pagination.getPrevPageProducts();
