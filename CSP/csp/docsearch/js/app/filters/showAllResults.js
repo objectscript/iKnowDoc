4eddbb082953f7eb7177de571fe928b2a7687f5a
manzhula <manzhula1994@gmail.com>
2017-05-19 17:08:23 +1000

    подсветка

--- CSP/csp/docsearch/js/app/filters/showAllResults.js
+++ CSP/csp/docsearch/js/app/filters/showAllResults.js
@@ -10,17 +10,23 @@ searchApp.filter('showAllResults', function( $sce ){
        	if (!input || !input.length) return;
        	
        	for(var i = 0; i < input.length; i++){       		
-       		var content = input[i].text.replace(new RegExp(phrase, "gi" ), '<span class="Illumination"><b> $&'+'</b></span>');
+       		var content = input[i].text.replace(new RegExp(phrase, "gi" ), '<span class="Illumination"><b> $& </b></span>');
        		tempResults.push({
        			text: $sce.trustAsHtml(String(content)),
        			textKey: input[i].textKey
-       		})
+       		});
        	};
        	
        	if (hideToggle)
+       	{
      		results = tempResults.slice(first, last);
+     		//tempResults.length = 0;
+     	}
      	if(!hideToggle)
+     	{
      		results = tempResults;
+     		//tempResults.length = 0;
+     	}
      	
         return results;
     }
