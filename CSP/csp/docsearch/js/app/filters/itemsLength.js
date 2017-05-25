c3f44e67750b77043e573d4d2a56e2c21e99225c
Beduin <Beduin@192.168.23.64>
2017-05-25 21:13:41 +1000

    исправлено обрезание тегов подсветки

--- CSP/csp/docsearch/js/app/filters/itemsLength.js
+++ CSP/csp/docsearch/js/app/filters/itemsLength.js
@@ -2,16 +2,23 @@ var searchApp = angular.module('searchApp');
  
 	searchApp.filter('itemsLength', function(){
 	     
-	     return function(input, first, last, hideToggle, word){
-	       	
-	       	var template = '<span class = "Illumination">' + word + '</span>';
-	       	
-	       	var len = 
+	     return function(input, first, last, hideToggle){
+	       	var template =/<span class="Illumination">[\S]+/ig;
+	       	/*var template =new RegExp('<span class="Illumination">'+word+'<\/span>', "ig");*/
+	       	var len = template.length;
 	       	
 	     	if (!input || !input.length) return;
 	       	
-	       	if (hideToggle)
-	       		return input.splice(first, last);
+	       	if (hideToggle){
+	       		 var res;
+	       		 while ((res = template.exec(input)) != null) {
+	       		 	if(res.index<last&&last<template.lastIndex){
+	       		 	last=template.lastIndex;
+	       		 	break;
+	       		 	}
+	       		 }
+	       		return input.slice(first, last);
+	       	}
 	       	else
 	       		return input;
 	    }
