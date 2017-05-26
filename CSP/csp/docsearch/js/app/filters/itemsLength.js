a6670f52384261d8058c3da3df1c0105cbcb07f4
Beduin <Beduin@192.168.23.64>
2017-05-26 13:38:09 +1000

    запрет на обрезание строки посреди слова

--- CSP/csp/docsearch/js/app/filters/itemsLength.js
+++ CSP/csp/docsearch/js/app/filters/itemsLength.js
@@ -3,16 +3,15 @@ var searchApp = angular.module('searchApp');
 	searchApp.filter('itemsLength', function(){
 	     
 	     return function(input, first, last, hideToggle){
-	       	var template =/<span class="Illumination">[\S]+/ig;
-	       	/*var template =new RegExp('<span class="Illumination">'+word+'<\/span>', "ig");*/
+	       	var template =/[\S]+/ig;
 	       	var len = template.length;
 	       	
 	     	if (!input || !input.length) return;
 	       	
 	       	if (hideToggle){
-	       		 var res;
-	       		 while ((res = template.exec(input)) != null) {
-	       		 	if(res.index<last&&last<template.lastIndex){
+	       		 var temp;
+	       		 while ((temp = template.exec(input)) != null) {
+	       		 	if(temp.index<last&&last<template.lastIndex){
 	       		 	last=template.lastIndex;
 	       		 	break;
 	       		 	}
