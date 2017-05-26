a4b4226ff2a5d4c3365f2d300d50f5456b40d28b
Beduin <Beduin@192.168.23.64>
2017-05-26 15:59:59 +1000

    изменено обрезание строк в результатах

--- CSP/csp/docsearch/js/app/filters/itemsLength.js
+++ CSP/csp/docsearch/js/app/filters/itemsLength.js
@@ -3,18 +3,23 @@ var searchApp = angular.module('searchApp');
 	searchApp.filter('itemsLength', function(){
 	     
 	     return function(input, first, last, hideToggle){
-	       	var template =/[\S]+/ig;
+	       	var template =/<span class="Illumination">[\S]+/ig;
+	       	var pattern =/[\S]+/ig;
 	       	var len = template.length;
-	       	
 	     	if (!input || !input.length) return;
 	       	
 	       	if (hideToggle){
 	       		 var temp;
-	       		 while ((temp = template.exec(input)) != null) {
-	       		 	if(temp.index<last&&last<template.lastIndex){
-	       		 	last=template.lastIndex;
-	       		 	break;
+	       		 while (((temp = template.exec(input)) != null) || ((temp = pattern.exec(input)) != null)) {
+	       		 	if((temp.index < last) && (last < template.lastIndex)){
+	       		 		last=template.lastIndex;
+	       		 		break;
 	       		 	}
+	       		 	if((temp.index < last) && (last < pattern.lastIndex)){
+	       		 		last=pattern.lastIndex;
+	       		 		break;
+	       		 	}
+	       		 	if((template.lastIndex > last) && (pattern.lastIndex > last)) break;
 	       		 }
 	       		return input.slice(first, last);
 	       	}
